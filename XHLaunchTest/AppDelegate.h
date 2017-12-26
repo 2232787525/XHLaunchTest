27b13e2bbd2313e5932128022c0a19de26a6008a
2232787525 <bex@bex.local>
2017-12-26 14:23:19 +0800

    Initial Commit

---
+++ XHLaunchTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHLaunchTest
+//
+//  Created by bex on 2017/12/26.
+//  Copyright © 2017年 bex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHLaunchTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XHLaunchTest
+//
+//  Created by bex on 2017/12/26.
+//  Copyright © 2017年 bex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
